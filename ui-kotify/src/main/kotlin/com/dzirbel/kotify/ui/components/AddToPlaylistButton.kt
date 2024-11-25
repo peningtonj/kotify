8cd7d39115e189ac5f1ff70cee1af873a6655952
Joseph Penington <josephpenington@Josephs-Laptop.local>
2024-11-25 16:19:03 +1100

    add album to playlist button

---
+++ ui-kotify/src/main/kotlin/com/dzirbel/kotify/ui/components/AddToPlaylistButton.kt
@@ -0,0 +1,41 @@
+package com.dzirbel.kotify.ui.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import com.dzirbel.kotify.repository.SavedRepository
+import com.dzirbel.kotify.ui.CachedIcon
+import com.dzirbel.kotify.ui.theme.Dimens
+import com.dzirbel.kotify.ui.theme.KotifyColors
+import com.dzirbel.kotify.ui.util.instrumentation.instrument
+
+/**
+ * A generic button to toggle the saved state of the entity with the given [id] via the given [repository].
+ */
+@Composable
+fun ToggleSaveButton(
+    repository: SavedRepository,
+    id: String,
+    modifier: Modifier = Modifier,
+    size: Dp = Dimens.iconSmall,
+) {
+    val savedState = repository.savedStateOf(id = id).collectAsState().value
+    IconButton(
+        modifier = modifier.instrument().size(size),
+        enabled = savedState is SavedRepository.SaveState.Set,
+        onClick = {
+            check(savedState is SavedRepository.SaveState.Set)
+            repository.setSaved(id = id, saved = !savedState.saved)
+        },
+    ) {
+        CachedIcon(
+            name = if (savedState?.saved == true) "favorite" else "favorite-border",
+            size = size,
+            contentDescription = "Save",
+            tint = KotifyColors.highlighted(savedState?.saved == true),
+        )
+    }
+}
